75a7593d1e1ad24c4dbb74fe8da222ec407bf2a6
sayantanmandal009 <157054653+sayantanmandal009@users.noreply.github.com>
2025-01-10 00:24:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "Hello {first}\n\n<b>Please Click on 'JOIN CHANNEL' button and Join,\nThen click on 'TRY AGAIN' button to get your file</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>[TG:-@PicXTV] {previouscaption}</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>[TG:-@Chalochitra] {previouscaption}</b>")
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
