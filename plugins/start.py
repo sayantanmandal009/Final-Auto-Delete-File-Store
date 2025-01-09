e2a10c828c79fee89fe3fd050ad47b7450e90611
jbeil0540 <jeibel6060@gmail.com>
2025-01-09 23:07:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -62,10 +62,15 @@ async def start_command(client: Client, message: Message):
 
         for msg in messages:
 
-            if bool(CUSTOM_CAPTION) & bool(msg.Video):
-                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.Video.file_name)
+            if bool(CUSTOM_CAPTION) & bool(msg.video):
+                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.video.file_name)
             else:
                 caption = "" if not msg.caption else msg.caption.html
+           
+            if bool(CUSTOM_CAPTION) & bool(msg.document):
+                caption = CUSTOM_CAPTION.format(previouscaption = "" if not msg.caption else msg.caption.html, filename = msg.document.file_name)
+            else:
+                caption = "" if not msg.caption else msg.caption.html                
 
             if DISABLE_CHANNEL_BUTTON:
                 reply_markup = msg.reply_markup
